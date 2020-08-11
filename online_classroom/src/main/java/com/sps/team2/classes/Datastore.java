a874944112cf21c565f233b53d8340ec7668c6b4
nicole <nicolercl1103@gmail.com>
2020-08-11 22:37:02 +0800

    Remove redundant imports

--- online_classroom/src/main/java/com/sps/team2/classes/Datastore.java
+++ online_classroom/src/main/java/com/sps/team2/classes/Datastore.java
@@ -6,10 +6,6 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.FilterOperator;
-import com.google.appengine.api.datastore.Query.SortDirection;
-import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
 
 import java.util.*;
 
