6e1903480584317257787859ec75905da3fb3d22
nicole <nicolercl1103@gmail.com>
2020-08-11 22:38:59 +0800

    Remove redundant imports

--- online_classroom/src/main/java/com/sps/team2/servlets/RegisterServlet.java
+++ online_classroom/src/main/java/com/sps/team2/servlets/RegisterServlet.java
@@ -1,7 +1,5 @@
 package com.sps.team2.servlets;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import java.io.IOException;
